f67c43b66836c5e61677d209ffd4e5fcda4d18ff
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-09-12 14:02:51 +0300

    Add modules containing ascii art and words, check if letter is already guessed

--- main.py
+++ main.py
@@ -1,84 +1,37 @@
 import random
+import hangman_art
+import hangman_words
+print(hangman_art.logo)
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
 
-lives  = 6
 
-word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+lives = 6
+
+chosen_word = random.choice(hangman_words.word_list)
 
 print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
 display += "_" * len(chosen_word)
+guessed_letters = []
 
 while "_" in display and lives > 0:
     guess = input("Guess a letter: ").lower()
-    if guess in chosen_word:
-        position = 0
-        for letter in chosen_word:
-            if guess == letter:
-                display[position] = letter
-            position += 1
+    if guess in guessed_letters:
+        print(f"You've already guesses {guess}")
     else:
-        lives -= 1
+        guessed_letters += guess
+        if guess in chosen_word:
+            position = 0
+            for letter in chosen_word:
+                if guess == letter:
+                    display[position] = letter
+                position += 1
+        else:
+            lives -= 1
+            print(f"You guessed {guess}, that's not in the word. You lose a life.")
     print(f"{' '.join(display)}")
-    print(stages[lives])
+    print(hangman_art.stages[lives])
 if lives == 0:
     print("You lose")
 else:
