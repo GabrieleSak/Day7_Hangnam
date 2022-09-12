21f2939ff4262558e17f833171320da5aadc1509
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-09-12 11:31:58 +0300

    Lets user guess again while no more "_" in display

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ chosen_word = random.choice(word_list)
 
 print(f'Pssst, the solution is {chosen_word}.')
 
-guess = input("Guess a letter: ").lower()
-
 display = []
 display += "_" * len(chosen_word)
 
-position = 0
+while "_" in display:
+    guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
-    if guess == letter:
-        display[position] = letter
-    position += 1
+    position = 0
+    for letter in chosen_word:
+        if guess == letter:
+            display[position] = letter
+        position += 1
 
-print(display)
+    print(display)
+print("You win")
